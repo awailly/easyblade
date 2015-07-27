82b450fb2c8963200e62d5fc0c4fde6111cf7b8e
Aurélien Wailly <aurelien.wailly@gmail.com>
2015-07-27 15:07:29 +0200

    Fix unbootable image format

--- create-deb8.sh
+++ create-deb8.sh
@@ -1,2 +1,2 @@
 # http://cdimage.debian.org/cdimage/openstack/
-glance image-create --name "Debian 8 - `date +%F`" --disk-format raw --min-disk 8 --min-ram 2048 --location "http://cdimage.debian.org/cdimage/openstack/8.1.0/debian-8.1.0-openstack-amd64.raw" --is-public True --is-protected False --progress
+glance image-create --name "Debian 8 - `date +%F`" --disk-format qcow2 --min-disk 8 --min-ram 2048 --location "http://cdimage.debian.org/cdimage/openstack/8.1.0/debian-8.1.0-openstack-amd64.qcow2" --is-public True --is-protected False --progress
