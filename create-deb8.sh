1e6b91fe0e653e3a54a631a752d2e86cd4f944a1
Aurélien Wailly <aurelien.wailly@gmail.com>
2015-07-27 14:01:13 +0200

    Add debian 8 glance creator

---
+++ create-deb8.sh
@@ -0,0 +1,1 @@
+glance image-create --name "Debian 8 - `date +%F`" --disk-format raw --min-disk 8 --min-ram 2048 --location "http://cdimage.debian.org/cdimage/openstack/8.1.0/debian-8.1.0-openstack-amd64.raw" --is-public True --is-protected False --progress
