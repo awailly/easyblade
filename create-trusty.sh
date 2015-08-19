afd1220fcd489e42e6939a3c5c7b5e515f232934
Aurélien Wailly <aurelien.wailly@gmail.com>
2015-08-19 16:57:48 +0200

    Add container

--- create-trusty.sh
+++ create-trusty.sh
@@ -1,2 +1,2 @@
 # https://cloud-images.ubuntu.com/trusty/current/
-glance2 image-create --name "Trusty 64b - `date +%F`" --disk-format raw --min-disk 8 --min-ram 2048 --location "https://cloud-images.ubuntu.com/trusty/current/trusty-server-cloudimg-amd64-disk1.img" --is-public True --is-protected False --progress
+glance2 image-create --name "Trusty 64b - `date +%F`" --disk-format raw --min-disk 8 --min-ram 2048 --location "https://cloud-images.ubuntu.com/trusty/current/trusty-server-cloudimg-amd64-disk1.img" --container-format bare --is-public True --is-protected False --progress
